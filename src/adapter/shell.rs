616366817cb1378d5e52e1157d93056995753e8c
Jimmy Cuadra <jimmy@jimmycuadra.com>
2017-03-05 02:19:14 -0800

    Add a prompt and exit commands to the shell adapter.

--- src/adapter/shell.rs
+++ src/adapter/shell.rs
@@ -1,5 +1,6 @@
-use std::io::{self, BufRead};
+use std::io::{self, BufRead, Write};
 use std::thread;
+use std::time::Duration;
 
 use futures::future::{err, ok};
 use futures::sync::mpsc::channel;
@@ -52,21 +53,53 @@ impl Adapter for Shell {
 
         thread::spawn(move || {
             let input = io::stdin();
+            let mut output = io::stdout();
+            let prompt = "Rustin > ";
+            let duration = Duration::from_millis(10);
+            let mut dirty_exit = true;
+
+            println!("Type \"exit\" or \"quit\" to end the session.");
+            print!("{}", prompt);
+            output.flush().unwrap();
 
             for line in input.lock().lines() {
                 match line {
                     Ok(body) => {
+                        if body.len() == 0 {
+                            print!("{}", prompt);
+                            output.flush().unwrap();
+
+                            continue;
+                        } else if body == "exit" || body == "quit" {
+                            dirty_exit = false;
+
+                            break;
+                        }
+
                         let user = User::new("1", Some("Shell User"));
                         let source = Source::User(user);
                         let message = IncomingMessage::new(source, body);
 
                         match tx.send(Ok(message)).wait() {
-                            Ok(new_tx) => tx = new_tx,
+                            Ok(new_tx) => {
+                                // Hack to keep the prompt from appearing before handlers have
+                                // finished responding.
+                                thread::sleep(duration);
+
+                                tx = new_tx;
+
+                                print!("{}", prompt);
+                                output.flush().unwrap();
+                            }
                             Err(_) => break,
                         }
                     }
                     Err(_) => break,
                 }
+            }
+
+            if dirty_exit {
+                println!();
             }
         });
 
