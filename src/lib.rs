9a907f5c07b2b072e158b040df0d1815b93f4f40
Jimmy Cuadra <jimmy@jimmycuadra.com>
2017-08-19 23:28:59 -0700

    Add tests.

--- src/lib.rs
+++ src/lib.rs
@@ -20,3 +20,62 @@ pub use error::Error;
 pub use robot::{Builder, Robot};
 pub use room::Room;
 pub use user::User;
+
+#[cfg(test)]
+mod tests {
+    use futures::future::ok;
+    use futures::stream::{empty, once};
+    use futures::{Future, Stream};
+
+    use super::chat_service::ChatService;
+    use super::message::{IncomingMessage, OutgoingMessage};
+    use super::storage::Memory;
+    use super::{Action, Callback, Error, Robot, Room};
+
+    #[derive(Clone, Debug)]
+    struct NullChat;
+
+    impl ChatService for NullChat {
+        fn join(&self, _room: &Room) -> Box<Future<Item = (), Error = Error>> {
+            Box::new(ok(()))
+        }
+
+        fn part(&self, _room: &Room) -> Box<Future<Item = (), Error = Error>> {
+            Box::new(ok(()))
+        }
+
+        fn send_message(&self, _message: OutgoingMessage) -> Box<Future<Item = (), Error = Error>> {
+            Box::new(ok(()))
+        }
+
+        fn incoming(&self) -> Box<Stream<Item = IncomingMessage, Error = Error>> {
+            Box::new(empty())
+        }
+    }
+
+    #[test]
+    fn manual_stateless_callback() {
+        struct Echo;
+
+        impl Callback for Echo {
+            fn call(&self, message: IncomingMessage) -> Box<Stream<Item = Action, Error = Error>> {
+                Box::new(once(Ok(message.reply(message.body()))))
+            }
+        }
+
+        Robot::build(NullChat, Memory::new())
+            .callback(Echo)
+            .finish();
+    }
+
+    #[test]
+    fn fn_stateless_callback() {
+        fn echo(message: IncomingMessage) -> Box<Stream<Item = Action, Error = Error>> {
+            Box::new(once(Ok(message.reply(message.body()))))
+        }
+
+        Robot::build(NullChat, Memory::new())
+            .callback(echo)
+            .finish();
+    }
+}
