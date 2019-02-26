85f11b664b94d245f737e8ca27796af2a2219050
Jimmy Cuadra <jimmy@jimmycuadra.com>
2019-02-26 03:17:41 -0800

    Add an alias field in Config.

--- src/config.rs
+++ src/config.rs
@@ -3,14 +3,21 @@
 /// Configuration data for a `Robot`.
 #[derive(Clone, Debug)]
 pub struct Config {
-    /// The name of the robot as displayed to human users.
-    pub name: String,
+    /// An alias for the robot.
+    ///
+    /// The robot normally determines that a message is directed to it by looking for its username
+    /// or display name at the beginning of a message. If an alias is set, its presence at the
+    /// beginning of the message will also be treated as a directed message.
+    ///
+    /// This is commonly set to a single character like `/` or `!` as a short way of sending
+    /// commands to the robot in chat.
+    pub alias: Option<String>,
 }
 
 impl Default for Config {
     fn default() -> Self {
         Config {
-            name: "Rustin".to_string(),
+            alias: None,
         }
     }
 }
